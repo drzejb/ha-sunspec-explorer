13fb6571008e8045e9aeb3662ac2dc127528899f
Develop43 <develop43@dev.int>
2026-07-24 08:21:12 +0200

    structure

---
+++ ha-sunspec-explorer/app/main.py
@@ -0,0 +1,9 @@
+import platform
+from datetime import datetime
+
+print("====================================")
+print("HA SunSpec Explorer")
+print("Version : 0.0.1")
+print("Python  :", platform.python_version())
+print("Started :", datetime.now())
+print("====================================")
---
+++ ha-sunspec-explorer/run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/with-contenv bashio
+
+bashio::log.info "===================================="
+bashio::log.info "HA SunSpec Explorer"
+bashio::log.info "Starting..."
+bashio::log.info "===================================="
+
+python3 /app/main.py
